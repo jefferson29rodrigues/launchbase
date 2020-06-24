d46223a139ab2cf31d2ff84790f741f9f2018894
Jefferson Rodrigues <jeffersonrodrigues5030@gmail.com>
2020-06-24 16:18:37 -0300

    projeto desafio2-3 completado com correção na execução do modal

--- desafios/desafio2/scripts.js
+++ desafios/desafio2/scripts.js
@@ -4,11 +4,11 @@ const modalOverlay = document.querySelector('.modal-overlay');
 const cards = document.querySelectorAll('.card');
 
 for (let card of cards) {
-    card.addEventListener("click", function() {
+    card.addEventListener("click", function(){
         const videoId = card.getAttribute("id");
         modalOverlay.classList.add('active');
-        modalOverlay.querySelector("iframe").src = `https://www.youtube.com/embed${videoId}`;
-    });
+        modalOverlay.querySelector("iframe").src = `https://www.youtube.com/embed/${videoId}`;
+    })
 }
 
 document.querySelector(".close-modal").addEventListener("click", function() {
