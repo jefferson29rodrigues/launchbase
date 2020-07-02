81ca6f8f0449cccc43cd00507e480c3b6cd80b78
Jefferson Rodrigues <jeffersonrodrigues5030@gmail.com>
2020-07-02 20:42:11 -0300

    server.js primeira rota criada!

--- desafios/desafio-3-a-/server.js
+++ desafios/desafio-3-a-/server.js
@@ -2,6 +2,11 @@ const express = require('express');
 
 const server = express();
 
+
+server.get("/", function(req, res) {
+    return res.send('Primeira rota criada');
+});
+
 server.listen(5000, function() {
     console.log("Servidor rodando!")
 });
