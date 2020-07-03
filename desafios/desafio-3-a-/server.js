d917f6e8a864cbf63ac07ae50c2dbad4b9f6151a
Jefferson Rodrigues <jeffersonrodrigues5030@gmail.com>
2020-07-03 01:11:42 -0300

    instalado nodemon para restart automático de servidor

--- desafios/desafio-3-a-/server.js
+++ desafios/desafio-3-a-/server.js
@@ -4,7 +4,7 @@ const server = express();
 
 
 server.get("/", function(req, res) {
-    return res.send('Primeira rota criada');
+    return res.send('desafio2-3');
 });
 
 server.listen(5000, function() {
