ff0034cd61626d83a4428fbc8c3ddd2ae34f5f26
Jefferson Rodrigues <jeffersonrodrigues5030@gmail.com>
2020-07-03 01:29:16 -0300

    server setado, nunjucks configurado e res.render retornando página

--- desafios/desafio-3-a-/server.js
+++ desafios/desafio-3-a-/server.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const nunjucks = require('nunjucks');
 
 const server = express();
 
+server.set("view engine", "html");
+
+nunjucks.configure('views', {
+    express: server
+});
 
 server.get("/", function(req, res) {
-    return res.send('desafio2-3');
+    return res.render('desafio2-3');
 });
 
 server.listen(5000, function() {
