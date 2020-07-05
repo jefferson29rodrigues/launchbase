2f19c456a838737d6bb2cf8a050d540998ea2e37
Jefferson Rodrigues <jeffersonrodrigues5030@gmail.com>
2020-07-05 01:45:30 -0300

    concluido desafio 3-2

---
+++ desafios/desafio-3-a-/data.js
@@ -0,0 +1,44 @@
+module.exports = [
+    {
+        id: "-VQPimwkstM",
+        img: "Starter.64c237cc",
+        title: "Curso Starter para impulsionar seu ponto de partida!",
+        duration: "50 horas",        
+        price: "Grátis"
+    },
+    {
+        id: "U7GjS3FuSkA",
+        img: "LaunchBase.08285320",
+        title: "Seja Programador Júnior!",
+        duration: "250 horas",
+        price: "1.000,00"
+    },
+    {
+        id: "SXSKvUB9wDA",
+        img: "GoStack.83a178a0",
+        title: "Seja desejado pelas empresas!",
+        duration: "300 horas",
+        price: "3.000,00"
+    },
+    {
+        id: "SXSKvUB9wDA",
+        img: "GoStack.83a178a0",
+        title: "Seja desejado pelas empresas!",
+        duration: "300 horas",
+        price: "3.000,00"
+    },
+    {
+        id: "U7GjS3FuSkA",
+        img: "LaunchBase.08285320",
+        title: "Seja Programador Júnior!",
+        duration: "250 horas",
+        price: "1.000,00"
+    },
+    {
+        id: "-VQPimwkstM",
+        img: "Starter.64c237cc",
+        title: "Curso Starter para impulsionar seu ponto de partida!",
+        duration: "50 horas",        
+        price: "Grátis"
+    }
+]
--- desafios/desafio-3-a-/server.js
+++ desafios/desafio-3-a-/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const nunjucks = require('nunjucks');
 
 const server = express();
+const cursos = require("./data");
 
 server.use(express.static('public'));
 
@@ -16,7 +17,7 @@ server.get("/", function(req, res) {
 });
 
 server.get("/courses", function(req, res) {
-    return res.render('courses');
+    return res.render('courses', { items: cursos });
 });
 
 server.use(function(req, res) {
