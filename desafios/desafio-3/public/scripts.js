a257087ab8c670f26659c68d2321ceb059dd71d6
Jefferson Rodrigues <jeffersonrodrigues5030@gmail.com>
2020-07-11 15:31:48 -0300

    courses.njk redirecionando pelo id

--- desafios/desafio-3/public/scripts.js
+++ desafios/desafio-3/public/scripts.js
@@ -1,4 +1,6 @@
 
+/*
+
 
 const modalOverlay = document.querySelector('.modal-overlay');
 const cards = document.querySelectorAll('.card');
@@ -9,16 +11,35 @@ for (let card of cards) {
         modalOverlay.classList.add('active');
         modalOverlay.querySelector("iframe").src = `https://www.youtube.com/embed/${videoId}`;/*
         modalOverlay.querySelector("iframe").src = `https://rocketseat.com.br/${videoId}`;*/
-    })
+    /*})
 }
 
 document.querySelector(".close-modal").addEventListener("click", function() {
     modalOverlay.classList.remove("active");
     modalOverlay.querySelector("iframe").src = "";
-});
+});*/
 
 /*
 document.querySelector('modal').addEventListener('click', function() {
     modalOverlay.classList.contains
 });
 */      // Falta funcionalidade de maximizar o MODAL
+
+
+
+
+
+
+
+
+
+const modalOverlay = document.querySelector('.modal-overlay');
+const cards = document.querySelectorAll('.card');
+
+for (let card of cards) {
+    card.addEventListener("click", function(){
+        const cursoId = card.getAttribute("id");
+        
+        window.location.href = `/description?id=${ cursoId }`;
+    })
+}
